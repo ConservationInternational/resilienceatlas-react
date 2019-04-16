84599d83ae8efda4fa1b0140ac5de93cb5d81e27
David Inga <david.inga@vizzuality.com>
2019-04-16 11:24:49 +0200

    added memory use env variable

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,7 +36,7 @@ set :linked_files, %w{.env ecosystem.config.js}
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 2
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
@@ -52,9 +52,9 @@ set :pm2_start_params, '--env production'
 
 # Yarn
 # set :yarn_target_path, -> { release_path.join('subdir') } # default not set
-# set :yarn_flags, '--production --silent --no-progress'    # default
+set :yarn_flags, '--silent --no-progress'
 # set :yarn_roles, :all                                     # default
-# set :yarn_env_variables, {}                               # default
+set :yarn_env_variables, { 'NODE_OPTIONS': '--max-old-space-size=2048' }
 
 namespace :deploy do
 
@@ -62,7 +62,8 @@ namespace :deploy do
   task :build_app do
     on roles(:app) do
       within release_path do
-        execute :yarn, 'build'
+        execute :npm, 'run build', '-- --max-old-space-size=2048'
+        # execute :yarn, 'build'
       end
     end
   end
