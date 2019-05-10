0a509157cd962bb52d536668093e992d072b0d1d
David Inga <david.inga@vizzuality.com>
2019-05-10 12:13:41 +0200

    fixing getActives selector

--- src/views/components/AnalysisPanel/AnalysisContent/AnalysisContent.container.js
+++ src/views/components/AnalysisPanel/AnalysisContent/AnalysisContent.container.js
@@ -3,13 +3,9 @@ import { getActives } from '@modules/layers';
 
 import { BarChartsList } from './AnalysisContent.component';
 
-const mapStateToProps = state => {
-  const actives = getActives();
-
-  return {
-    activeLayers: actives(state),
-    geojson: state.map.geojson,
-  };
-};
+const mapStateToProps = state => ({
+  activeLayers: getActives(state),
+  geojson: state.map.geojson,
+});
 
 export default connect(mapStateToProps)(BarChartsList);
