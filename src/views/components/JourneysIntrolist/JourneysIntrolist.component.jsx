81eb467aff467169a5fe986da936a5e40ea6ccaa
maria <maluenarod@gmail.com>
2019-05-16 10:17:37 +0200

    journeys data added

--- src/views/components/JourneysIntrolist/JourneysIntrolist.component.jsx
+++ src/views/components/JourneysIntrolist/JourneysIntrolist.component.jsx
@@ -9,33 +9,34 @@ const JourneysIntrolist = ({ journeys, loadJourneys }) => {
 
   return (
     <ul className="m-journey__grid">
-      {journeys.map((j, i) => (
-        <li
-          className={cx('m-journey__gridelement', j['bg-big'])}
-          data-index={i}
-          key={j.id}
-        >
-          <div className="text">
-            <span className="title">
-              <h2>
-                <NavLink to={`/journeys/${j.id}`}>{j.title}</NavLink>
-              </h2>
-            </span>
-            <span className="description">
-              <p>{j.theme}</p>
-            </span>
-            <NavLink
-              to={`/journeys/${j.id}`}
-              className="btn btn-secondary theme-bg-color"
-            >
-              Learn more
+      {journeys.data &&
+        journeys.data.map((j, i) => (
+          <li
+            className={cx('m-journey__gridelement', j['bg-big'])}
+            data-index={i}
+            key={j.id}
+          >
+            <div className="text">
+              <span className="title">
+                <h2>
+                  <NavLink to={`/journeys/${j.id}`}>{j.title}</NavLink>
+                </h2>
+              </span>
+              <span className="description">
+                <p>{j.theme}</p>
+              </span>
+              <NavLink
+                to={`/journeys/${j.id}`}
+                className="btn btn-secondary theme-bg-color"
+              >
+                Learn more
+              </NavLink>
+            </div>
+            <NavLink to={`/journeys/${j.id}`} className="pic">
+              <span className="background" />
             </NavLink>
-          </div>
-          <NavLink to={`/journeys/${j.id}`} className="pic">
-            <span className="background" />
-          </NavLink>
-        </li>
-      ))}
+          </li>
+        ))}
     </ul>
   );
 };
