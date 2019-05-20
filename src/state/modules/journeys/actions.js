ee409617ce1ab1caee8830febcd1c8c2b7e60485
maria <maluenarod@gmail.com>
2019-05-20 17:23:18 +0200

    get journeys url adadpted

--- src/state/modules/journeys/actions.js
+++ src/state/modules/journeys/actions.js
@@ -1,12 +1,11 @@
 import api, { createApiAction, PORT } from '../../utils/api';
 
 const URL_JOURNEYS = '/journeys';
-const URL_JOURNEYS_PAGE_INDEX = '/journeys/journeysPageIndex.json';
 
 // Action constants
 export const LOAD_PAGE_INDEX = createApiAction('journeys/LOAD_PAGE_INDEX');
 
 export const loadPageIndex = () =>
   api(LOAD_PAGE_INDEX, ({ get }) =>
-    get(URL_JOURNEYS_PAGE_INDEX, { baseURL: `${PORT}/api` }),
+    get(URL_JOURNEYS, { baseURL: `${PORT}/api` }),
   );
