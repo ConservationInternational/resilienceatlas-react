9245d1cde7715e3a76b551bcd5d514ded4b9f6d9
maria <maluenarod@gmail.com>
2019-05-21 15:16:28 +0200

    current step added to journey reducer + selector

--- src/state/modules/journey/reducer.js
+++ src/state/modules/journey/reducer.js
@@ -1,11 +1,17 @@
 import { createReducer } from '../../utils';
-import { LOAD_JOURNEY_INDEX } from './actions';
+import { LOAD_JOURNEY_INDEX, SET_CURRENT_STEP } from './actions';
 
-const initialState = {};
+const initialState = {
+  currentStep: 0,
+};
 
 export default createReducer(initialState)({
   [LOAD_JOURNEY_INDEX.SUCCESS]: (state, { payload }) => ({
     ...state,
     ...payload.data[0],
+  }),
+  [SET_CURRENT_STEP]: (state, { payload }) => ({
+    ...state,
+    currentState: payload,
   }),
 });
--- src/state/modules/journey/selectors.js
+++ src/state/modules/journey/selectors.js
@@ -1,3 +1,4 @@
 import { createSelector } from 'reselect';
 
 export const getSteps = state => state.journey;
+export const currentStep = state => state.journey.curentStep;
