dd7bc5e4c7188123e6cc97c18df47498a2cacaa6
maria <maluenarod@gmail.com>
2019-05-22 16:12:36 +0200

    simplifying component - journey landing slide

--- src/views/components/JourneyLanding/JourneyLanding.component.jsx
+++ src/views/components/JourneyLanding/JourneyLanding.component.jsx
@@ -1,33 +1,20 @@
-import React, { useEffect } from 'react';
-import JourneyControls from '../JourneyControls/JourneyControls.container';
+import React from 'react';
 
-const JourneyLanding = ({ landingInfo, loadJourneyInfo }) => {
-  useEffect(() => {
-    loadJourneyInfo();
-  }, []);
-
-  if (!landingInfo) return null;
-  const journey = landingInfo[0];
-  return (
-    <div className={`m-journey--landing is-stretch ${journey.background}`}>
+const JourneyLanding = ({ title, background, subtitle, theme }) => (
+  <div className="l-journey__intro" id="journeyIndexView">
+    <div className={`m-journey--landing is-stretch ${background}`}>
       <div className="row">
         <div className="large-12">
           <div className="intro">
-            <h1>{journey.title}</h1>
-            <h2>{journey.subtitle}</h2>
-            <h3>{journey.theme}</h3>
+            <h1>{title}</h1>
+            <h2>{subtitle}</h2>
+            <h3>{theme}</h3>
           </div>
         </div>
       </div>
-      <p className="credits">
-        <a target="_blank" rel="noopener noreferrer" href={journey.creditsUrl}>
-          {journey.credits}.
-        </a>
-      </p>
-      <JourneyControls />
       <div className="logo" />
     </div>
-  );
-};
+  </div>
+);
 
 export default JourneyLanding;
--- src/views/components/JourneyLanding/index.js
+++ src/views/components/JourneyLanding/index.js
@@ -1,1 +1,1 @@
-export { default } from './JourneyLanding.container';
+export { default } from './JourneyLanding.component';
