89f0d77518b1d2e949142433390fe66b808fc60c
Alex Malinin <amalinin.codica@gmail.com>
2019-05-27 16:50:33 +0300

    [Feature] add predictive model layer to legend

--- src/views/components/Legend/Legend.container.js
+++ src/views/components/Legend/Legend.container.js
@@ -1,17 +1,39 @@
+import qs from 'qs';
+import { compose } from 'redux';
 import { connect } from 'react-redux';
-
+import { withRouter } from 'react-router-dom';
 import {
   getActives,
   reorder,
   toggle as toggleLayer,
   setOpacity,
 } from '@modules/layers';
+import { makeLayer as makePredictiveModelLayer } from '@modules/predictive_models';
+
+import { TABS } from '../Sidebar';
+
 import Legend from './Legend.component';
 
-const mapStateToProps = state => ({
-  activeLayers: getActives(state),
-  loading: state.layers.actives.length > 0 && state.layers.loading,
-});
+const makeMapStateToProps = () => {
+  const getPredictiveModelLayer = makePredictiveModelLayer();
+
+  const mapStateToProps = (state, { location }) => {
+    const { tab } = qs.parse(location.search, { ignoreQueryPrefix: true });
+
+    if (tab === TABS.MODELS) {
+      return {
+        activeLayers: [getPredictiveModelLayer(state)].filter(Boolean),
+        loading: state.predictive_models.loading,
+      };
+    }
+
+    return {
+      activeLayers: getActives(state),
+      loading: state.layers.actives.length > 0 && state.layers.loading,
+    };
+  };
+  return mapStateToProps;
+};
 
 const mapDispatchToProps = {
   reorder,
@@ -19,7 +41,12 @@ const mapDispatchToProps = {
   setOpacity,
 };
 
-export default connect(
-  mapStateToProps,
+const withConnect = connect(
+  makeMapStateToProps,
   mapDispatchToProps,
+);
+
+export default compose(
+  withRouter,
+  withConnect,
 )(Legend);
