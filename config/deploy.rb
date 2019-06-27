41bb4aa069de4fb540bb8bf022c7f4395dcab55d
David Inga <david.inga@vizzuality.com>
2019-06-27 09:10:35 +0200

    changed repository url for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.4.0"
 
 set :application, "ResilienceAtlasReact"
-set :repo_url, "git@github.com:Vizzuality/resilience-atlas-react.git"
+set :repo_url, "https://github.com/Vizzuality/resilience-atlas-react.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
