34c793df12787a28b5e18c2d66e48e115ea83357
David Inga <david.inga@vizzuality.com>
2019-07-01 17:26:30 +0200

    disabling devtools only for development

--- src/state/store.js
+++ src/state/store.js
@@ -1,10 +1,10 @@
-import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import * as reducers from './modules';
 import normalizrMiddleware from './middlewares/normalizrMiddleware';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 
-// eslint-disable-next-line no-underscore-dangle
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancer = composeWithDevTools({});
 
 export default createStore(
   combineReducers(reducers),
